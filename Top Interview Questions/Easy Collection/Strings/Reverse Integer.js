ea91fda40bd71d47018a39f02f120218b597cf22
ULemong <86288617+ULemong@users.noreply.github.com>
2021-12-05 00:51:33 +0900

    solution

--- Top Interview Questions/Easy Collection/Strings/Reverse Integer.js
+++ Top Interview Questions/Easy Collection/Strings/Reverse Integer.js
@@ -1,8 +1,12 @@
 const reverse = function (x) {
-  // reverse 시 0으로 시작하고, 2자 이상일 때
-  const a = String(x).split('').reverse().join('');
-  const regExp = /[^0]/g;
-  console.log(a.match(regExp));
-};
+  if (!x) return 0;
 
-console.log(reverse(-102));
+  const arr = String(x).split('').reverse();
+  if (arr[arr.length - 1] === '-') {
+    arr.unshift('-');
+    arr.pop();
+  }
+  if (+arr.join('') < (-2) ** 31 || arr.join('') > 2 ** 31 - 1) return 0;
+  if (arr[0] === '0') return +arr.join('').slice(1);
+  return +arr.join('');
+};
