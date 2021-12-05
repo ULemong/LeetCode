2fd8e3bdcf829ca80b77ba5418238ebdd3b4cbb0
ULemong <86288617+ULemong@users.noreply.github.com>
2021-12-05 23:58:49 +0900

    solution

---
+++ Top Interview Questions/Medium Collection/Linked List/Intersection of Two Linked Lists.js
@@ -0,0 +1,32 @@
+const getIntersectionNode = function(headA, headB) {
+    
+  let curA = headA;
+  let curB = headB;
+  let lenA = 0;
+  let lenB = 0;
+      while (curA !== null) {
+          lenA += 1
+          curA = curA.next
+      }
+          
+      while (curB !== null) {
+          lenB += 1
+          curB = curB.next
+      }
+          
+      curA= headA;
+      curB = headB;
+      if (lenA > lenB) {
+          for (let i = 0; i < lenA-lenB; i++) curA = curA.next;          
+      }
+          
+      else if (lenB > lenA)
+          for (let i = 0; i < lenB-lenA; i++) curB = curB.next;
+              
+      while (curB !== curA) {
+          curB = curB.next;
+          curA = curA.next;
+      }
+          
+      return curA;
+};
